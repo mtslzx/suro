cac5a991ff70f7b07d722cb90ffe34aa81bfa30d
mtslzx <35260883+mtslzx@users.noreply.github.com>
2020-02-21 12:57:10 +0900

    TwitchDownloader 프로젝트를 Suro 프로젝트에 적용

--- suro.py
+++ suro.py
@@ -9,7 +9,7 @@ https://stackoverflow.com/questions/42901942/how-do-we-download-a-blob-url-video
 https://m.blog.naver.com/shino1025/221279112390
 
 - 사용된 라이브러리 -
-time , random , requests
+time , random , requests , moviepy
 
 
 = 트위치 구독자 전용 VOD 다운로더 b0.1 =
@@ -18,6 +18,26 @@ time , random , requests
 - 변경점 -
 [b0.1]
 최초 프로그램 기초 작성
+
+[b0.2]
++ File 접근 및 다운로드 기능
+
+[b0.3]
++ Url 구분 기능
+
+[b0.4]
++ 순차 다운로드 기능
+
+[1.0]
+정식 릴리즈
++ 봇 감지 방지를 위한 슬립기능
++ 다운로드 실패시 알림
+
+[1.1]
+Twitch Downloader 프로젝트 합병
+
+[1.1.1]
+·moviepy 기능 테스트
 
 
 '''
