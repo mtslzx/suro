24d7605a9720507f70819721f28d59571f8873f4
mtslzx <35260883+mtslzx@users.noreply.github.com>
2020-02-26 03:35:09 +0900

    FFMPEG 라이브러리 테스트

---
+++ dev/test/ffmpeg_test.py
@@ -0,0 +1,26 @@
+import os
+loop = 0
+
+if __name__ == '__main__':
+    mv_name = str(input("영상 제목을 입력해주세요:"))
+    loop = int(input("마지막 영상 번호를 입력하세요(0 - n):"))
+    print(str(loop) + ' 입력확인됨.')
+
+dl_list = open('download_list.txt', 'w')  # 텍스트 파일 생성
+
+for i in range(loop + 1):
+    dl_list.write("file '%s.ts'\n"% i)  # file '0.ts' 형식으로 저장
+
+dl_list.close()  # 파일 저장 후 닫기
+
+
+# 현재 경로 저장
+os.system('cd {}'.format(os.getcwd()))
+output = os.popen('dir').read()
+print(output)
+
+# FFMPEG / .ts 파일 합치기
+os.system("ffmpeg -f concat -i download_list.txt -c copy %s.ts" % mv_name)
+
+# FFMPEG / .ts To .mp4
+os.system("ffmpeg -i %s.ts -acodec copy -vcodec copy %s.mp4" % (mv_name, mv_name))
